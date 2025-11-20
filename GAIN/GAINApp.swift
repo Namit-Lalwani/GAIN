726f96e43cab9ba1e007006a3ee2314677ecc13d
RUSTY <NAMIT02082005@GMAIL.COM>
2025-11-20 16:33:23 +0530

    Initial Commit

---
+++ GAIN/GAINApp.swift
@@ -0,0 +1,17 @@
+//
+//  GAINApp.swift
+//  GAIN
+//
+//  Created by RUSTY on 20/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GAINApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
